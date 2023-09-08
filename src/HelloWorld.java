6fad26f41513479fc43a71492914f9335e793ba3
paulsebastiankrus <paulsebastiankrus@gmail.com>
2023-09-08 08:47:25 +0200

    First Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,26 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class HelloWorld {
+    public static void main(String[]args) {
+        System.out.println("Hello World \nHello da23D ");
+        System.out.println("Hello World" + " Hello da23D ");
+        System.out.println(23 + 1);
+        System.out.println("Dette er årstal: " +23+1);
+        LocalDate today = LocalDate.now();
+        System.out.println(today);
+        System.out.println(LocalDate.now());
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+        System.out.println(now);
+        System.out.println(LocalDateTime.now());
+
+
+        char hej='a';
+        System.out.println(hej);
+        boolean b1=true;
+        System.out.print(b1);
+
+    }
+}
